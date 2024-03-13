df89d8a18cc378f5fcf6a2b0674b31d6ff2020b7
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-03-13 17:14:20 +0000

    First coomit

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,80 @@
-//import react into the bundle
-import React from "react";
+import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom";
-
-
-// include your styles into the webpack bundle
+import PropTypes from "prop-types";
 import "../styles/index.css";
 
-
-
-
-
-function SimpleCounter(props){
-    return (
-        <div className="bigCounter">
-            <div className="calendar">
-                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
-                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
-                    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
-                </svg>
-            </div>
-            <div className="number">0</div>
-            <div className="number">0</div>
-            <div className="number">0</div>
-            <div className="number">0</div>
-        </div>
-    );
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        {/* SVG code remains unchanged */}
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
 }
 
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+};
 
+function App() {
+  const [isCounting, setIsCounting] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const [clickCount, setClickCount] = useState(0); // New state for click count
+  const intervalIdRef = useRef(null);
 
-//render your react application
-ReactDOM.render(
-<SimpleCounter />, document.querySelector("#app")
-);
+  const handleStart = () => {
+    if (!isCounting) {
+      intervalIdRef.current = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+      setIsCounting(true);
+    }
+  };
+
+  const handleStop = () => {
+    if (isCounting) {
+      clearInterval(intervalIdRef.current);
+      setIsCounting(false);
+    }
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalIdRef.current);
+    setIsCounting(false);
+    setCounter(0);
+    setClickCount(0); // Also reset click count
+  };
+
+  const handleClickCount = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setClickCount((prevCount) => prevCount + 1); // Increment click count
+  };
+
+  return (
+    <div>
+      <SimpleCounter
+        digitOne={counter % 10}
+        digitTwo={Math.floor(counter / 10) % 10}
+        digitThree={Math.floor(counter / 100) % 10}
+        digitFour={Math.floor(counter / 1000) % 10}
+      />
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleClickCount}>Click to Count</button>
+      {/* Display click counter */}
+      <p>Clicks: {clickCount}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
+
