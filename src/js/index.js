55535aff0223c130424dabcf23407e88c47e9b9e
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-03-13 17:36:30 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,10 @@ function SimpleCounter(props) {
   return (
     <div className="bigCounter">
       <div className="calendar">
-        {/* SVG code remains unchanged */}
+        {<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+</svg>}
       </div>
       <div className="four">{props.digitFour % 10}</div>
       <div className="three">{props.digitThree % 10}</div>
